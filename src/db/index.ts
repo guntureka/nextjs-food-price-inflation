d6f60e3b8d086f6baee6cfaab0042e043475e790
guntureka <guntureka1020@gmail.com>
2025-03-09 06:30:24 +0700

    push

--- src/db/index.ts
+++
@@ -1,9 +0,0 @@
-import { drizzle } from "drizzle-orm/node-postgres";
-import * as schema from "@/db/schema";
-
-export const db = drizzle({
-  connection: {
-    connectionString: process.env.DATABASE_URL!,
-  },
-  schema,
-});
