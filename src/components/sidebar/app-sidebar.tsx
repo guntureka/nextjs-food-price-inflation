feea32a6c451686c2565c95b3f66a2236921535a
Guntur Eka <guntureka1020@gmail.com>
2025-06-21 21:21:19 +0700

    feat

--- src/components/sidebar/app-sidebar.tsx
+++ src/components/sidebar/app-sidebar.tsx
@@ -3,7 +3,6 @@
 import {
   DropdownMenu,
   DropdownMenuContent,
-  DropdownMenuGroup,
   DropdownMenuItem,
   DropdownMenuLabel,
   DropdownMenuSeparator,
@@ -22,7 +21,6 @@ import {
   useSidebar,
 } from "@/components/ui/sidebar";
 import {
-  BadgeCheck,
   Banana,
   ChevronsUpDown,
   DollarSign,
@@ -151,12 +149,19 @@ export function AppSidebar({ session }: AppSidebarProps) {
                   className="cursor-pointer data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
                 >
                   <Avatar className="h-8 w-8 rounded-lg">
-                    <AvatarImage src={"/next.svg"} alt={"avatar"} />
+                    <AvatarImage
+                      src={session.user.image ?? "/next.svg"}
+                      alt={"avatar"}
+                    />
                     <AvatarFallback className="rounded-lg">CN</AvatarFallback>
                   </Avatar>
                   <div className="grid flex-1 text-left text-sm leading-tight">
-                    <span className="truncate font-semibold">{"Gunture"}</span>
-                    <span className="truncate text-xs">{"guntureka"}</span>
+                    <span className="truncate font-semibold">
+                      {session.user.name}
+                    </span>
+                    <span className="truncate text-xs">
+                      {session.user.email}
+                    </span>
                   </div>
                   <ChevronsUpDown className="ml-auto size-4" />
                 </SidebarMenuButton>
@@ -170,26 +175,22 @@ export function AppSidebar({ session }: AppSidebarProps) {
                 <DropdownMenuLabel className="p-0 font-normal">
                   <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                     <Avatar className="h-8 w-8 rounded-lg">
-                      <AvatarImage src={"/next.svg"} alt={"Guntur Eka"} />
+                      <AvatarImage
+                        src={session.user.image ?? "/next.svg"}
+                        alt={"avatar"}
+                      />
                       <AvatarFallback className="rounded-lg">CN</AvatarFallback>
                     </Avatar>
                     <div className="grid flex-1 text-left text-sm leading-tight">
                       <span className="truncate font-semibold">
-                        {"Guntur Eka"}
+                        {session.user.name}
                       </span>
                       <span className="truncate text-xs">
-                        {"guntureka1020@gmail.com"}
+                        {session.user.email}
                       </span>
                     </div>
                   </div>
                 </DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuGroup>
-                  <DropdownMenuItem>
-                    <BadgeCheck />
-                    Account
-                  </DropdownMenuItem>
-                </DropdownMenuGroup>
                 <DropdownMenuSeparator />
                 <DropdownMenuItem
                   variant="destructive"
